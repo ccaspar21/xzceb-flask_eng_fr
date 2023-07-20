2a31280df3fc78aeb396a064558af692400b49fa
ccaspar21 <136540119+ccaspar21@users.noreply.github.com>
2023-07-20 13:06:44 -0400

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,1 +1,16 @@
+from deep_translator import MyMemoryTranslator
 
+def english_to_french(eng_text_input):
+  """
+  Converts english to french
+  """
+  fr_text_output = MyMemoryTranslator(source='english',target='french').translate(eng_text_input)
+  return fr_text_output
+
+def french_to_english(fr_text_input):
+  """
+  Converts english to french
+  """
+  eng_text_output = MyMemoryTranslator(source='french',target='english').translate(fr_text_input)
+  return eng_text_output
+  
