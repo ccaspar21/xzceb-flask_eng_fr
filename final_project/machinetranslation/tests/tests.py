1e7bf008bd723c567dba78162ff89797224611ce
ccaspar21 <136540119+ccaspar21@users.noreply.github.com>
2023-07-20 13:03:48 -0400

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,1 +1,14 @@
+import unittest
+from translator import english_to_french, french_to_english
 
+class TestTranslateEnglishToFrench(unittest.TestCase):
+  def test1(self):
+    self.assertEqual(english_to_french("Hello"), "Bonjour")
+    self.assertNotEqual(english_to_french("Bonjour"), "English")
+
+class TestTranslateFrenchToEnglish(unittest.TestCase):
+  def test1(self):
+    self.assertEqual(french_to_english("Hello"), "Bonjour")
+    self.assertNotEqual(french_to_english("Bonjour"), "English")
+
+unittest.main()
